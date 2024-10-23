b09e5b092cd99a93a9aa055898de36e4e77624f2
Artem-Mo <m0gilevskiy@yandex.ru>
2024-10-23 16:27:13 +0300

    [+]: graphql schema + resolvers generated

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/mvp-mogila/ozon-test-task/internal/app"
+)
+
+func main() {
+	log.Println("Application init")
+	app := app.NewApp()
+
+	log.Println("Start application")
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
